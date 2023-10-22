645f9f156381c9922c9e47a9336b8522b1a7dc52
Hussein Tilly <hussein.tilly@gmail.com>
2023-10-22 10:59:03 +0200

    updated helloWorld js to accept input

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,16 @@
-console.log("Git is Awesome!");
+// Create an input element
+var inputElement = document.createElement("input");
+inputElement.setAttribute("type", "text");
+inputElement.setAttribute("id", "inputField");
+
+// Create a button element
+var buttonElement = document.createElement("button");
+buttonElement.textContent = "Log to Console";
+buttonElement.addEventListener("click", function () {
+  var inputValue = inputElement.value;
+  console.log("You entered: " + inputValue);
+});
+
+// Append the input and button elements to the document body
+document.body.appendChild(inputElement);
+document.body.appendChild(buttonElement);
